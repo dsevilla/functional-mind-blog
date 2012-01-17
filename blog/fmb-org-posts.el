971805765b279e3d1df548933837917eaf3e7822
Diego Sevilla Ruiz <dsevilla@ditec.um.es>
2012-01-17 02:44:16 +0100

    More work.

--- blog/fmb-org-posts.el
+++ blog/fmb-org-posts.el
@@ -3,7 +3,8 @@
 
 (eval-when-compile
   (require 'cl)
-  (require 'org-exp))
+  (require 'org-exp)
+  (require 'fmb-blog))
 
 ; org-infile-export-plist
 ; #+TITLE:
@@ -26,11 +27,11 @@
       (cond
        ;; check to see whether filename ends in `.org'
        ;; and if so, append its name to a list.
-       ((equal ".org" (substring (car (car current-directory-list)) -4))
+       ((equal ".org" (substring (caar current-directory-list) -4))
         (setq org-files-list
-              (cons (car (car current-directory-list)) org-files-list)))
+              (cons (caar current-directory-list) org-files-list)))
        ;; check whether filename is that of a directory
-       ((eq t (car (cdr (car current-directory-list))))
+       ((eq t (cadar current-directory-list))
         ;; decide whether to skip or recurse
         (if
             (equal "."
@@ -42,12 +43,34 @@
           (setq org-files-list
                 (append
                  (fmb-files-in-below-directory
-                  (car (car current-directory-list)))
+                  (caar current-directory-list))
                  org-files-list)))))
       ;; move to the next filename in the list; this also
       ;; shortens the list so the while loop eventually comes to an end
       (setq current-directory-list (cdr current-directory-list)))
     ;; return the filenames
     org-files-list))
+
+(defun fmb-import-org-posts ()
+  "Search all .org files, create a buffer for each of them,
+extract all the initial properties (export properties), and then generate
+the HTML equivalent of the body. Add it to the list of actual posts, that
+will be ordered by date finally."
+  (dolist (f (fmb-files-in-below-directory "content/posts"))
+    (with-current-buffer (find-file-noselect f)
+      (let* ((file-properties (org-infile-export-plist))
+             (title (plist-get file-properties :title))
+             (date (plist-get file-properties :date))
+             (categories (plist-get file-properties :keywords))
+             (body-as-html (org-export-region-as-html (point-min)
+                                                      (point-max)
+                                                      t 'string))
+                                        ; date
+                                        ; title
+                                        ; categories
+             )
+        (fmb-new-post title :date date :categories categories
+                      :body body-as-html)
+      (kill-buffer))))
 
 (provide 'fmb-org-posts)
